e2a9a4afef62626870dd84e72920c6d9c27a0f65
Yash Zade <yashmzade@gmail.com>
2025-09-27 02:04:50 +0530

    Add virtual token

---
+++ contracts/src/tokens/VEth.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VUSDTToken is ERC20, Ownable {
+    constructor() ERC20("Virtual ETH", "vETH") Ownable(msg.sender) {}
+
+    mapping(address => bool) public hasClaimed;
+
+    function mint(address to, uint _amount) public onlyOwner {
+        _mint(to, _amount);
+    }
+
+    function airDrop(address to) public {
+        require(!hasClaimed[to], "Already claimed");
+        _mint(to, 10000 * (10 ** decimals()));
+        hasClaimed[to] = true;
+    }
+
+    function burn(address _from, uint256 _amount) public onlyOwner {
+        _burn(_from, _amount);
+    }
+}
