a43a4f8c822cab0284a7c79c18c479ac8a7d4170
himanxhu1233 <dhandolehimanshu@gmail.com>
2025-09-27 14:38:26 +0530

    setup done

--- frontend/StealthSDk/src/App.tsx
+++ frontend/StealthSDk/src/App.tsx
@@ -1,4 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { config } from "./config/WagmiConfig.ts";
 
 import IndexPage from "@/pages/index";
 import DocsPage from "@/pages/docs";
@@ -7,14 +10,20 @@ import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<IndexPage />} path="/" />
+          <Route element={<DocsPage />} path="/docs" />
+          <Route element={<PricingPage />} path="/pricing" />
+          <Route element={<BlogPage />} path="/blog" />
+          <Route element={<AboutPage />} path="/about" />
+        </Routes>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
